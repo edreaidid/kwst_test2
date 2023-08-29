923780b24d616cc52a396ddf47faeee27b8c9985
Edre Mohammad Aidid <61385353+edreaidid@users.noreply.github.com>
2023-08-29 11:56:47 +0800

    Update 6_others.py

--- pages/6_others.py
+++ pages/6_others.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 st.write("HUBUNGI KONSULTAN KAMI UNTUK BANTUAN LANJUT")
 
+st.info(" #### Admin monitoring :point_down:")
+    password = st.text_input("Enter a password", value="", type="password")
+    if password=="abcd":
+        st.dataframe(pd.read_csv("kwst.csv",names=["staf sasmec","bergejala", "ahli staf", "ahli bergejala", "masa"]),height=300)
